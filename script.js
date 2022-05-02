44082bcfb836c0d3d18cbc2ce67c6de35d5d52e4
Rimma Anikevich <ranikevich@gmail.com>
2022-05-02 21:28:55 +0700

    Added the ability to change the color of the circle and its size depending on input

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const button = document.querySelector('#btn');
+const text = document.getElementById('text');
+const square = document.querySelector('#square');
+const circleButton = document.querySelector('#e_btn');
+const circle = document.querySelector('#circle');
+const range = document.getElementById('range');
+const rangeSpan = document.getElementById('range-span');
+
+
+const changeColor = function () {
+    square.style.backgroundColor = text.value;
+};
+
+const logger = function (event) {
+    rangeSpan.textContent = event.target.value;
+    circle.style.width = parseInt(rangeSpan.textContent)+'%';
+    circle.style.height = parseInt(rangeSpan.textContent)+'%';
+};
+
+button.addEventListener('click', changeColor);
+circleButton.style.display = 'none';
+
+range.addEventListener('input', logger);
 
